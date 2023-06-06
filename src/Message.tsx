eef4f255d7f81ac0ed166f09ae56e84a200dc3a4
Ayush Senapati <ayushsenapati123@gmail.com>
2023-06-06 21:54:46 +0530

    Trying out Message Component

---
+++ src/Message.tsx
@@ -0,0 +1,10 @@
+//! PascalCasing - a general convention where we keep the 1st char of function name as a capital letter
+
+function Message() {
+  // JSX: JavaScript XML -> Converts the HTML code to JS code
+  // We can also dynamically pass something which returns something
+  const name = "Ayush Senapati";
+  return <h1>Hello {name}</h1>;
+}
+
+export default Message;
