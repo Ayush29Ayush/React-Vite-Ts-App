9dd80d52cb8b4e8dcb514b7afd6660cd4d9f8623
Ayush Senapati <ayushsenapati123@gmail.com>
2023-06-07 14:51:21 +0530

    Rendering Lists dynamically using array & map func

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,16 +1,21 @@
 // import { Fragment } from "react";
 
 function ListGroup() {
+  const items = ["Delhi", "Bhubaneshwar", "Kolkata", "Gurugram"];
+
   return (
     // <Fragment></Fragment>
     <>
       <h1>My List</h1>
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
+        {/* <li className="list-group-item">An item</li>
         <li className="list-group-item">A second item</li>
         <li className="list-group-item">A third item</li>
         <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        <li className="list-group-item">And a fifth one</li> */}
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
       </ul>
     </>
   );
