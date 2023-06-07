d57da9075c6655f78346ceb5c6a731a843b7f70b
Ayush Senapati <ayushsenapati123@gmail.com>
2023-06-07 21:42:51 +0530

    refactor - Added Conditional Rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,37 @@
 // import { Fragment } from "react";
 
 function ListGroup() {
-  const items = ["Delhi", "Bhubaneshwar", "Kolkata", "Gurugram"];
+  // const items = ["Delhi", "Bhubaneshwar", "Kolkata", "Gurugram"];
+  let items = ["Delhi", "Bhubaneshwar", "Kolkata", "Gurugram"];
+  items = [];
+
+  // Normal Rendering
+  // if (items.length === 0)
+  //   return (
+  //     <>
+  //       <h1>List Items</h1>
+  //       <p>No items found</p>
+  //     </>
+  //   );
+
+  // const message = items.length === 0 ? <p>No item found</p> : null;
+
+  //! We can also use a function to get the correct output
+  //! Benifits of using function is that we can have parameters
+  // const getMessage = () => {
+  //   return items.length === 0 ? <p>No item found</p> : null
+  // }
 
   return (
     // <Fragment></Fragment>
     <>
-      <h1>My List</h1>
+      <h1>My List Heading</h1>
+      {/* //! Conditional Rendering */}
+      {/* { items.length===0 ? <p>No item found</p> : null } */}
+      {/* //! We can also use AND gate logic i.e (1 && 'A' => 'A') */}
+      {items.length === 0 && <p>No item found</p>}
+      {/* {message} */}
+      {/* {getMessage()} */}
       <ul className="list-group">
         {/* <li className="list-group-item">An item</li>
         <li className="list-group-item">A second item</li>
